6d941be366b5e81fcd4d88ae9e55b9179ff23c2a
Lybov1303 <lr.zadalya@mail.ru>
2021-07-14 22:46:18 +0300

    Second Commit

---
+++ AcademyHome/src/by/home/lesson1/Shuffel.java
@@ -0,0 +1,20 @@
+package by.home.lesson1;
+public class Shuffel {
+	public static void main(String[] arg) {
+		int x = 3;
+		while (x > 0) {
+			if (x > 2) {
+				System.out.print("a");
+			}
+			x = x - 1;
+			System.out.print("-");
+			if (x == 2) {
+				System.out.print("b c");
+			}
+			if (x == 1) {
+				System.out.print("d");
+				x = x - 1;
+			}
+		}
+	}
+}
