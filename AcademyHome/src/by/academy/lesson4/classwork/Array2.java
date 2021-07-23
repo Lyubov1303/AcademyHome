9b85130dd9f5161ffa53a503fddd205eef75ed62
Lybov1303 <lr.zadalya@mail.ru>
2021-07-24 00:50:12 +0300

    Массивы

--- AcademyHome/src/by/academy/lesson4/classwork/Array2.java
+++ AcademyHome/src/by/academy/lesson4/classwork/Array2.java
@@ -4,6 +4,6 @@ public class Array2 {
 	
 		public static void main(String[] args) {
 			int[] monthDays = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-			System.out.println("B апреле " + monthDays[3] + " дней.");
+			System.out.println("B Р°РїСЂРµР»Рµ" + monthDays[3] + " РґРЅРµР№.");
      }
 }
