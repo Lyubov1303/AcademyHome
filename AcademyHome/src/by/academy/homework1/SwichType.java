88943f8071ef22dfc3cda506a3c59b347a1a15d1
Lyubov1303 <lr.zadalya@mail.ru>
2021-08-17 20:25:59 +0300

    Lesson12

--- AcademyHome/src/by/academy/homework1/SwichType.java
+++ AcademyHome/src/by/academy/homework1/SwichType.java
@@ -4,27 +4,32 @@ import java.util.Scanner;
 public class SwichType {
 
 	public static void main(String[] args) {
-		System.out.println("Введите тип переменной: ");
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: ");
 		Scanner scanner = new Scanner(System.in);
 		String t = scanner.nextLine();
-		System.out.println("Введите переменную: ");
-		int x = scanner.nextInt();
+		System.out.println("пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ: ");
+		//int x = scanner.nextInt();
 
 		switch (t) {
 		case ("int"):
-			System.out.println(x % 2);
+			int a = scanner.nextInt();
+			System.out.println(a % 2);
 			break;
 		case ("double"):
-			System.out.println(x * 0.7);
+			double b = scanner.nextDouble();
+			System.out.println(b * 0.7);
 			break;
 		case ("float"):
-			System.out.println(x * x);
+			float c = scanner.nextFloat();
+			System.out.println(c * c);
 			break;
 		case ("char"):
-			System.out.println((char) x);
+			char d =  (char) scanner.nextInt();
+			System.out.println((char) d);
 			break;
 		case ("String"):
-			System.out.println("Hello " + x);
+			String e = scanner.next();
+			System.out.println("Hello " + e);
 			break;
 		default:
 			System.out.println("Unsupported type");
