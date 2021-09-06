387f7adcee1c262875e55fd0da0f945092ab885b
Lyubov1303 <lr.zadalya@mail.ru>
2021-09-06 19:42:26 +0300

    Homework7.Task1

--- AcademyHome/src/by/academy/homework7/Task1.java
+++ AcademyHome/src/by/academy/homework7/Task1.java
@@ -1,7 +1,7 @@
 package by.academy.homework7;
 
-
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -13,11 +13,18 @@ public class Task1 {
 
 		Supplier<Long> s = () -> (long) new Random().nextInt(seed);
 
-		return Stream.generate(s).limit(size).collect(Collectors.toList());
+		return Stream.generate(s).limit(size).peek(i -> i = (long) (i * Math.PI) - 20).filter(n -> n < 100).sorted()
+				.skip(3).collect(Collectors.toList());
 	}
+
 	public static void main(String... args) {
 
 		List<Long> longList = generateLongList(100, 100);
-			
-}
+
+		Map<Long, String> map = longList.stream()
+				.collect(Collectors.toMap(i -> i, key -> "Number: " + key, (i, key) -> i));
+
+		System.out.println(map);
+
+	}
 }
